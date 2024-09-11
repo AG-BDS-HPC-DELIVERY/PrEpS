f3520d60dc049b0b12296ec838b725e29daa6108
Nicolas Tallet <33759510+nicolas-tallet@users.noreply.github.com>
2024-09-11 17:45:24 +0200

    Add Modules for Managing cpufreq Driver

---
+++ libexec/preps/cpufreq/check_driver.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#-------------------------------------------------------------------------------
+## @file
+## @fn preps::cpufreq::check_driver()
+## @brief Check cpufreq Driver
+## @param num_cpus Total Number of CPUs
+## @return Return Code
+## @retval 0 cpufreq Driver is Operational
+## @retval 1 cpufreq Driver is Dysfunctional
+## @ingroup cpufreq
+#-------------------------------------------------------------------------------
+preps::cpufreq::check_driver() {
+	main::log_event -level "TRACE" -message "Entering Module: [${FUNCNAME[0]}]"
+	local -i rc=0
+	local num_cpus="${1}"
+	[[ -n "${num_cpus}" ]] || main::log_event -level "FATAL" -message "Missing Argument: [Number of CPUs]"
+	(( num_cpus > 0 )) || main::log_event -level "FATAL" -message "Invalid Number of CPUs: [${num_cpus}]"
+	count=$(find /sys/devices/system/cpu/ -maxdepth 1 -mindepth 1 -regex "/sys/devices/system/cpu/cpu[0-9]+" | wc -l)
+	(( count == num_cpus )) || \
+		{ main::log_event -level "ERROR" -message "Invalid Number of Visible CPUs - cpufreq Driver Must Be Dysfunctional"; rc=1; } 
+	main::log_event -level "TRACE" -message "Exiting Module: [${FUNCNAME[0]}] -> Return Code: [${rc}]"
+	return ${rc}
+}
