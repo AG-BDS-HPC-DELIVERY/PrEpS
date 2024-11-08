d314d1acad6ff180e4bd6d4d520c5530fd25d44c
Nicolas Tallet <33759510+nicolas-tallet@users.noreply.github.com>
2024-11-08 09:38:59 +0100

    Update Comments

--- libexec/preps/nvidia/cleanup_mps_processes.sh
+++ libexec/preps/nvidia/cleanup_mps_processes.sh
@@ -12,12 +12,12 @@
 preps::nvidia::cleanup_mps_processes() {
 	main::log_event -level "TRACE" -message "Entering Module: [${FUNCNAME[0]}]"
 	local -i rc=0
-	# MPS Processes
+	# Stop MPS Servers
 	if pgrep -f "nvidia-cuda-mps" &>/dev/null; then
 		echo quit | nvidia-cuda-mps-control
 		main::log_event -level "INFO" -message "Stopped CUDA MPS Servers"
 	fi
-	# MPS Zombie Processes
+	# Kill MPS Residual Processes
 	if pgrep -f "nvidia-cuda-mps" &>/dev/null; then
 		if killall --signal SIGKILL nvidia-cuda-mps-server &>/dev/null; then
 			main::log_event -level "INFO" -message "Killed Residual CUDA MPS Servers Processes"
