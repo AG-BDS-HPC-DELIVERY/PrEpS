5a4d22c20bf3cf1376c347a46d4b2192d32a7d1d
Nicolas Tallet <33759510+nicolas-tallet@users.noreply.github.com>
2024-12-17 10:41:13 +0100

    Minor Change

--- libexec/apis/nvsmi/get_gpu_count.sh
+++ libexec/apis/nvsmi/get_gpu_count.sh
@@ -11,9 +11,7 @@
 apis::nvsmi::get_gpu_count() {
 	local gpu_count=0
 	gpu_count="$(${NVSMI_EXECBIN} --format=csv,noheader,nounits --query-gpu=count 2>/dev/null | head --lines=1)"
-	if [[ "${gpu_count}" == "No devices were found" ]]; then
-		gpu_count=0
-	fi
+	[[ "${gpu_count}" == "No devices were found" ]] && gpu_count=0
 	echo "${gpu_count}"
 	return 0
 }
